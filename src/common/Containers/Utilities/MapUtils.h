808857d8cb03f19170f16fb08c3582af2da370cb
Shauren <shauren.trinity@gmail.com>
2025-02-23 11:33:53 +0100

    Core/Utils: Added map key/value utility projections for std::ranges algorithms

--- src/common/Containers/Utilities/MapUtils.h
+++ src/common/Containers/Utilities/MapUtils.h
@@ -47,5 +47,16 @@ void MultimapErasePair(M<K, V, Rest...>& multimap, K const& key, V const& value)
             ++itr;
     }
 }
+
+/**
+ * Map key projection for various std::ranges algorithms
+ */
+inline constexpr auto MapKey = []<typename Pair>(Pair&& pair) constexpr -> decltype(auto) { return (std::forward<Pair>(pair).first); /*Parentheses required for decltype(auto) to deduce a reference*/ };
+
+/**
+ * Map value projection for various std::ranges algorithms
+ */
+inline constexpr auto MapValue = []<typename Pair>(Pair&& pair) constexpr -> decltype(auto) { return (std::forward<Pair>(pair).second); /*Parentheses required for decltype(auto) to deduce a reference*/
+};
 }
 #endif // TRINITYCORE_MAP_UTILS_H
