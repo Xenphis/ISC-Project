jairoe05/tc_duplicate

Core/Packets: Added SMSG_NEW_WORLD and CMSG_MOVE_WORLDPORT_ACK Also fixed wrong glyph index define

src/server/game/Handlers/MovementHandler.cpp
@@ -36,7 +36,7 @@
 
 #define MOVEMENT_PACKET_TIME_DELAY 0
 
-void WorldSession::HandleMoveWorldportAckOpcode(WorldPacket & /*recvData*/)
+void WorldSession::HandleMoveWorldportAckOpcode(WorldPackets::Movement::WorldPortAck& /*packet*/)
 {
     TC_LOG_DEBUG("network", "WORLD: got MSG_MOVE_WORLDPORT_ACK.");
     HandleMoveWorldportAckOpcode();

Xenphis/ISC-Project

(cherry picked from commit 2580e7e30b21fdc46ea302761b08545d222fbaab)

src/server/game/Handlers/MovementHandler.cpp
@@ -182,7 +182,7 @@ uint32 WorldSession::AdjustClientMovementTime(uint32 time) const
         return uint32(movementTime);
 }
 
-void WorldSession::HandleMoveWorldportAckOpcode(WorldPacket & /*recvPacket*/)
+void WorldSession::HandleMoveWorldportAckOpcode(WorldPackets::Movement::WorldPortResponse& /*packet*/)
 {
     TC_LOG_DEBUG("network", "WORLD: got MSG_MOVE_WORLDPORT_ACK.");
     HandleMoveWorldportAck();